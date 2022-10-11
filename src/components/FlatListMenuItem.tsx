79abb61cf451f280969159297787a4253a3402e9
Zcehtro <stefano.n.giaimo@gmail.com>
2022-10-11 16:53:45 -0300

    added animation screens

--- src/components/FlatListMenuItem.tsx
+++ src/components/FlatListMenuItem.tsx
@@ -6,14 +6,19 @@ import { StyleSheet } from "react-native";
 import Icon from "react-native-vector-icons/Ionicons";
 import { Spacer } from "./Spacer";
 import { TouchableOpacity } from "react-native-gesture-handler";
+import { useNavigation } from "@react-navigation/native";
 
 interface Props {
   menuItem: MenuItem;
 }
 
 export const FlatListMenuItem = ({ menuItem }: Props) => {
+  const navigation = useNavigation();
+
   return (
-    <TouchableOpacity activeOpacity={0.8}>
+    <TouchableOpacity
+      activeOpacity={0.8}
+      onPress={() => navigation.navigate(menuItem.component as never)}>
       <View style={styles.container}>
         <Icon name={menuItem.icon} color="gray" size={23} />
         <Text style={styles.itemText}>{menuItem.name}</Text>
--- src/navigator/Navigator.tsx
+++ src/navigator/Navigator.tsx
@@ -1,3 +1,5 @@
+import { Animation101Screen } from "@app/screens/Animation101Screen";
+import { Animation102Screen } from "@app/screens/Animation102Screen";
 import { createStackNavigator } from "@react-navigation/stack";
 import { HomeScreen } from "../screens/HomeScreen";
 
@@ -10,6 +12,8 @@ export const Navigator = () => {
         headerShown: false,
       }}>
       <Stack.Screen name="HomeScreen" component={HomeScreen} />
+      <Stack.Screen name="Animation101Screen" component={Animation101Screen} />
+      <Stack.Screen name="Animation102Screen" component={Animation102Screen} />
     </Stack.Navigator>
   );
 };
