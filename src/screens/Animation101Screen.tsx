69515a863f1e66d2e4683f68d1cdd5385acaf746
Zcehtro <stefano.n.giaimo@gmail.com>
2022-10-11 16:56:40 -0300

    added fade animation

--- src/screens/Animation101Screen.tsx
+++ src/screens/Animation101Screen.tsx
@@ -1,9 +1,40 @@
-import { StyleSheet, View } from "react-native";
+import { useRef } from "react";
+import { StyleSheet, View, Animated, Button } from "react-native";
 
 export const Animation101Screen = () => {
+  const opacity = useRef(new Animated.Value(0.4)).current;
+
+  const fadeIn = () => {
+    Animated.timing(opacity, {
+      toValue: 1,
+      duration: 300,
+      useNativeDriver: true,
+    }).start(() => console.log("animación terminó"));
+  };
+
+  const fadeOut = () => {
+    Animated.timing(opacity, {
+      toValue: 0,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
+  const reset = () => {
+    Animated.timing(opacity, {
+      toValue: 0.4,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
     <View style={styles.container}>
-      <View style={{ ...styles.purpleBox }}></View>
+      <Animated.View
+        style={{ ...styles.purpleBox, opacity: opacity }}></Animated.View>
+      <Button title="FadeIn" onPress={fadeIn} />
+      <Button title="FadeOut" onPress={fadeOut} />
+      <Button title="Reset" onPress={reset} />
     </View>
   );
 };
