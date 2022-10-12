81ca41e1916ac6251a05d11f1bdb141d28c3bac9
Zcehtro <stefano.n.giaimo@gmail.com>
2022-10-12 16:22:20 -0300

    implemented switch

--- src/screens/SwitchScreen.tsx
+++ src/screens/SwitchScreen.tsx
@@ -1,12 +1,19 @@
-import { View } from "react-native"
+import { useState } from "react";
+import { View, Switch, Platform } from "react-native";
 
 export const SwitchScreen = () => {
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => setIsEnabled(!isEnabled);
+
   return (
-    <View
-    style={{marginTop: 100}}
-    >
-
-
+    <View style={{ marginTop: 100 }}>
+      <Switch
+        trackColor={{ false: "#D9D9DB", true: "#5856D6" }}
+        // thumbColor={Platform.OS === "android" ? "#5856D6" : ""}
+        // ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
     </View>
-  )
-}
+  );
+};
