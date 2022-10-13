6d1b7cb51517b418a8fd1765d6e232c5f1526f89
Zcehtro <stefano.n.giaimo@gmail.com>
2022-10-13 16:45:47 -0300

    added components to prevent keyboard from covering up screen different options implemented

--- src/screens/TextInputScreen.tsx
+++ src/screens/TextInputScreen.tsx
@@ -1,5 +1,14 @@
 import { useState } from "react";
-import { View, TextInput, StyleSheet } from "react-native";
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 
 import { HeaderTitle } from "@app/components/HeaderTitle";
 import { styles } from "@app/theme/appTheme";
@@ -18,29 +27,39 @@ export const TextInputScreen = () => {
     });
   };
   return (
-    <View style={styles.globalMargin}>
-      <HeaderTitle title="TextInputs" />
-      <TextInput
-        style={stylesScreen.inputStyle}
-        placeholder="Ingrese su nombre"
-        autoCorrect={false}
-        autoCapitalize="words"
-        onChangeText={value => onChange(value, "name")}
-        />
-      <TextInput
-        style={stylesScreen.inputStyle}
-        placeholder="Ingrese su email"
-        onChangeText={value => onChange(value, "email")}
-        keyboardType="email-address"
-        />
-      <TextInput
-        style={stylesScreen.inputStyle}
-        placeholder="Ingrese su teléfono"
-        onChangeText={value => onChange(value, "phone")}
-        keyboardType="phone-pad"
-      />
-      <HeaderTitle title={JSON.stringify(form, null, 3)} />
-    </View>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}>
+      <ScrollView>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.globalMargin}>
+            <HeaderTitle title="TextInputs" />
+            <TextInput
+              style={stylesScreen.inputStyle}
+              placeholder="Ingrese su nombre"
+              autoCorrect={false}
+              autoCapitalize="words"
+              onChangeText={value => onChange(value, "name")}
+            />
+            <TextInput
+              style={stylesScreen.inputStyle}
+              placeholder="Ingrese su email"
+              onChangeText={value => onChange(value, "email")}
+              keyboardType="email-address"
+            />
+            <HeaderTitle title={JSON.stringify(form, null, 3)} />
+            <HeaderTitle title={JSON.stringify(form, null, 3)} />
+            <TextInput
+              style={stylesScreen.inputStyle}
+              placeholder="Ingrese su teléfono"
+              onChangeText={value => onChange(value, "phone")}
+              keyboardType="phone-pad"
+            />
+            <HeaderTitle title={JSON.stringify(form, null, 3)} />
+            <View style={{ height: 100 }} />
+          </View>
+        </TouchableWithoutFeedback>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 };
 
