e5bfef15f404bd37330d696e5130d18010413799
Zcehtro <stefano.n.giaimo@gmail.com>
2022-10-13 13:06:17 -0300

    implemented prompt from external library

--- src/screens/AlertScreen.tsx
+++ src/screens/AlertScreen.tsx
@@ -1,4 +1,7 @@
 import { Alert, Button, View } from "react-native";
+
+import prompt from "react-native-prompt-android";
+
 import { HeaderTitle } from "@app/components/HeaderTitle";
 import { styles } from "@app/theme/appTheme";
 
@@ -21,11 +24,30 @@ export const AlertScreen = () => {
       },
     );
   };
+
+  const showPrompt = () => {
+    prompt(
+        'Enter password',
+        'Enter your password to claim your $1.5B in lottery winnings',
+        [
+         {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+         {text: 'OK', onPress: password => console.log('OK Pressed, password: ' + password)},
+        ],
+        {
+            type: 'secure-text',
+            cancelable: false,
+            defaultValue: 'test',
+            placeholder: 'placeholder'
+        }
+    );
+  }
   return (
     <View style={styles.globalMargin}>
       <HeaderTitle title="Alerts" />
 
       <Button title="Mostrar Alerta" onPress={showAlert} />
+
+      <Button title="Mostrar Prompt" onPress={showPrompt} />
     </View>
   );
 };
