b01568572a18e33e1befbc2437975f26b846f5b0
Zcehtro <stefano.n.giaimo@gmail.com>
2022-10-13 11:31:35 -0300

    multiple switches and arrow colours

--- src/components/FlatListMenuItem.tsx
+++ src/components/FlatListMenuItem.tsx
@@ -23,7 +23,7 @@ export const FlatListMenuItem = ({ menuItem }: Props) => {
         <Icon name={menuItem.icon} color="gray" size={23} />
         <Text style={styles.itemText}>{menuItem.name}</Text>
         <Spacer />
-        <Icon name={"chevron-forward-outline"} color="gray" size={23} />
+        <Icon name={"chevron-forward-outline"} color="#5856D6" size={23} />
       </View>
     </TouchableOpacity>
   );
--- src/screens/SwitchScreen.tsx
+++ src/screens/SwitchScreen.tsx
@@ -29,6 +29,20 @@ export const SwitchScreen = () => {
           onChange={value => onChange(value, "isActive")}
         />
       </View>
+      <View style={styles.switchRow}>
+        <Text style={styles.switchText}>isHungry</Text>
+        <CustomSwitch
+          isOn={isHungry}
+          onChange={value => onChange(value, "isHungry")}
+        />
+      </View>
+      <View style={styles.switchRow}>
+        <Text style={styles.switchText}>isHappy</Text>
+        <CustomSwitch
+          isOn={isHappy}
+          onChange={value => onChange(value, "isHappy")}
+        />
+      </View>
       <Text style={styles.switchText}>{JSON.stringify(state, null, 5)}</Text>
     </View>
   );
@@ -42,5 +56,6 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
+    marginVertical: 10,
   },
 });
