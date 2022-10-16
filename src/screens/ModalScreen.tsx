b130ecafce6909f4a91bdaee6f79c578c00b2555
Zcehtro <stefano.n.giaimo@gmail.com>
2022-10-16 18:34:20 -0300

    styling modal

--- src/screens/ModalScreen.tsx
+++ src/screens/ModalScreen.tsx
@@ -11,21 +11,34 @@ export const ModalScreen = () => {
 
       <Button title="Abrir modal" onPress={() => setIsVisible(true)} />
 
-      <Modal animationType="slide" visible={isVisible} transparent={true}>
+      <Modal animationType="fade" visible={isVisible} transparent={true}>
         {/* Background negro */}
         <View
           style={{
             flex: 1,
-            // height: 100,
-            // width: 100,
-            backgroundColor: 'rgba(0,0,0,0.5)',
+            backgroundColor: 'rgba(0,0,0,0.3)',
+            justifyContent: 'center',
+            alignItems: 'center',
           }}>
           <View
             style={{
               backgroundColor: 'white',
+              width: 200,
+              height: 200,
+              justifyContent: 'center',
+              alignItems: 'center',
+              shadowOffset: {
+                width: 0,
+                height: 10,
+              },
+              shadowOpacity: 0.25,
+              elevation: 10,
+              borderRadius: 20,
             }}>
-            <HeaderTitle title="Modal" />
-            <Text>Cuerpo del modal</Text>
+            <Text style={{ fontSize: 20, fontWeight: 'bold' }}>Modal</Text>
+            <Text style={{ fontSize: 16, fontWeight: '300', marginBottom: 20 }}>
+              Cuerpo del modal
+            </Text>
             <Button title="Cerrar" onPress={() => setIsVisible(false)} />
           </View>
         </View>
